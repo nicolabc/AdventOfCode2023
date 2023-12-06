3e3f13ec54514ce9c78431295a1a21295dc8e6bd
Nicolas B. Carbone <nicolasbcarbone@gmail.com>
2023-12-06 19:54:14 +0100

    Day5 part 2 optimized
    * optimized solution - still getting memoryexception on my personal machine (8gb ram) - works on stronger machine

--- Solutions/Day5.cs
+++ Solutions/Day5.cs
@@ -76,13 +76,77 @@ namespace Solutions
         public override int SecondQuestion(string filename)
         {
             var allLines = GetAllLines(filename);
-            return -1;
+            var almanac = new Almanac();
+            almanac.AllMaps.AddRange(
+            new List<List<Map>> {
+                almanac.SeedToSoilMaps,
+                almanac.SoilToFertilizerMaps,
+                almanac.FertilizerToWaterMaps,
+                almanac.WaterToLightMaps,
+                almanac.LightToTemperatureMaps,
+                almanac.TemperatureToHumidityMaps,
+                almanac.HumidityToLocationMaps,
+            });
+            var addCurrentLineToMap = false;
+            var CurrentAllMapsIndex = -1; //Start at -1 due to a gap between seeds and seeds-to-soil map
+            List<string> seedsRawAsStrings = new();
+            foreach (var line in allLines)
+            {
+                if (line.StartsWith("seeds: "))
+                {
+                    var splitted = line.Split("seeds: ")[1];
+                    seedsRawAsStrings = splitted.Split(" ").ToList();
+                }
+
+                if (line.Contains("map:"))
+                {
+                    addCurrentLineToMap = true;
+                    continue;
+                }
+
+                if (line == "")
+                {
+                    CurrentAllMapsIndex++;
+                    continue;
+                }
+
+                if (addCurrentLineToMap)
+                {
+                    almanac.AddLineToMap(line, almanac.AllMaps[CurrentAllMapsIndex]);
+                }
+            }
+
+            // Optimizing solution
+            var possibleLowestLocations = new List<long>();
+            var skippableLength = (long)0;
+            var lastSeedNotSkipped = (long)0;
+            for (var i = 0; i < seedsRawAsStrings.Count; i += 2)
+            {
+                var count = 0;
+                var rangeForSeed = long.Parse(seedsRawAsStrings[i + 1]);
+                var seedStart = long.Parse(seedsRawAsStrings[i]);
+                while (count < rangeForSeed)
+                {
+                    almanac.Seeds.Add(seedStart + count);
+                    count++;
+
+                    var currentSeed = seedStart + count;
+                    if (currentSeed < lastSeedNotSkipped + skippableLength && currentSeed > lastSeedNotSkipped) continue;
+                    (var location, skippableLength) = almanac.GetLocation(currentSeed, almanac.AllMaps);
+                    possibleLowestLocations.Add(location);
+                    lastSeedNotSkipped = currentSeed;
+                }
+            }
+
+            var lowestDestination = possibleLowestLocations.Min();
+            Console.WriteLine(lowestDestination);
+            return (int)lowestDestination;
         }
     }
 
     public class Almanac
     {
-        public List<long> Seeds { get; set; }
+        public List<long> Seeds { get; set; } = new();
         public List<Map> SeedToSoilMaps { get; set; } = new();
         public List<Map> SoilToFertilizerMaps { get; set; } = new();
         public List<Map> FertilizerToWaterMaps { get; set; } = new();
@@ -108,37 +172,54 @@ namespace Solutions
         public long GetLowestDestination()
         {
             var allLocations = new List<long>();
-            foreach (var seed in Seeds)
+            var skippableLength = (long)0;
+            var lastSeedNotSkipped = (long)0;
+            for (var i = 0; i < Seeds.Count; i++)
             {
-                allLocations.Add(GetLocation(seed, AllMaps));
+                var currentSeed = Seeds[i];
+                if (currentSeed < lastSeedNotSkipped + skippableLength && currentSeed > lastSeedNotSkipped)
+                {
+                    Console.WriteLine("Skipping!");
+                    continue;
+                };
+                (var location, skippableLength) = GetLocation(currentSeed, AllMaps);
+                allLocations.Add(location);
+                lastSeedNotSkipped = currentSeed;
             }
 
             return allLocations.Min();
         }
 
-        public long GetLocation(long seed, List<List<Map>> allMapsInOrder)
+        public (long, long) GetLocation(long seed, List<List<Map>> allMapsInOrder)
         {
             var currentPosition = seed;
+            long upperBoundSkippableDestination = long.MaxValue;
             foreach (var maps in allMapsInOrder)
             {
-                currentPosition = GetDestination(currentPosition, maps);
+                (currentPosition, var skippableLength) = GetDestination(currentPosition, maps);
+                if (skippableLength < upperBoundSkippableDestination) upperBoundSkippableDestination = skippableLength;
             }
 
-            return currentPosition;
+            return (currentPosition, upperBoundSkippableDestination);
         }
 
-        public long GetDestination(long input, List<Map> maps)
+        public (long, long) GetDestination(long input, List<Map> maps)
         {
+            var lowestHigherRangeStart = long.MaxValue;
             foreach (var map in maps)
             {
+
                 if (input >= map.SourceRangeStart && input < map.SourceRangeStart + map.RangeLength)
                 {
                     var transformationLength = input - map.SourceRangeStart;
-                    return map.DestinationRangeStart + transformationLength;
+                    var skippableLength = map.SourceRangeStart + map.RangeLength - input;
+                    return (map.DestinationRangeStart + transformationLength, skippableLength);
                 }
+
+                if (input < map.SourceRangeStart && map.SourceRangeStart < lowestHigherRangeStart) lowestHigherRangeStart = map.SourceRangeStart;
             }
 
-            return input;
+            return (input, lowestHigherRangeStart-input);
         }
     }
 
