4a41ac4608af1729f0fba94427f5a0c7e3599977
nicolabc <31611304+nicolabc@users.noreply.github.com>
2023-12-06 23:52:54 +0100

    Solved Day 2 Part 2

--- Solutions/Day2.cs
+++ Solutions/Day2.cs
@@ -70,8 +70,32 @@ namespace Solutions
 
         public override int SecondQuestion(string filename)
         {
-            var allLines = GetAllLines(filename);
-            return -1;
+            var allLines = GetAllLines(filename).ToList();
+            var sumOfPowers = 0;
+
+            for (var i = 0; i < allLines.Count; i++)
+            {
+                var samples = allLines[i].Split(":").ToList()[1];
+                string pattern = @"\;|,";
+                var rgx = new Regex(pattern);
+                var regexResult = rgx.Split(samples).ToList();
+
+                var colorValueSamples = new Dictionary<string, List<int>>()
+                {
+                    { "red", new List<int>() },
+                    { "green", new List<int>() },
+                    { "blue", new List<int>() },
+                };
+                foreach (var entry in regexResult)
+                {
+                    var valueColorPair = entry.Trim().Split(" ");
+                    colorValueSamples[valueColorPair[1]].Add(int.Parse(valueColorPair[0]));
+                }
+                sumOfPowers += FewestPossibleCubesPower(colorValueSamples);
+            }
+            return sumOfPowers;
         }
+
+        private int FewestPossibleCubesPower(Dictionary<string, List<int>> samples) => samples["green"].Max() * samples["red"].Max() * samples["blue"].Max();
     }
 }
