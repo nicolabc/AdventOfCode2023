fe3b3c23cf8cca51d5ee39907fe9c2a0b5656be8
nicolabc <31611304+nicolabc@users.noreply.github.com>
2023-12-08 00:28:11 +0100

    almost there!

--- Solutions/Day4.cs
+++ Solutions/Day4.cs
@@ -45,8 +45,35 @@ namespace Solutions
 
         public override int SecondQuestion(string filename)
         {
-            var allLines = GetAllLines(filename);
-            return -1;
+            var allLines = GetAllLines(filename).ToList();
+            var totalScratchCards = 0;
+            var gameIndex = 0;
+            RecursiveScratch(allLines, gameIndex, allLines, ref totalScratchCards);
+            return totalScratchCards;
+        }
+
+        private static int RecursiveScratch(List<string> subListLines, int gameIndex, List<string> allLines, ref int totalScratchCards)
+        {
+            gameIndex++;
+            for (var i = 0; i < subListLines.Count; i++)
+            {
+                var rgx = new Regex(@"\:|\|"); // Match ':' or '|'
+                var regexResult = rgx.Split(subListLines[i]).ToList();
+
+                var winningNumbers = regexResult[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var myNumbers = regexResult[2].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                var wins = 0;
+
+                for (var j = 0; j < myNumbers.Length; j++)
+                {
+                    if (winningNumbers.Contains(myNumbers[j])) wins++;
+                }
+
+                RecursiveScratch(allLines.Skip(gameIndex).Take(wins).ToList(), gameIndex, allLines, ref totalScratchCards);
+            }
+            totalScratchCards += 1; //Something with the count, the actual traversal is correct
+            return 0;
         }
     }
 }
