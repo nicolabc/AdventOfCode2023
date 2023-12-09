e7497eb515c159d273cdfdbb418afed6890fd3ff
nicolabc <31611304+nicolabc@users.noreply.github.com>
2023-12-09 20:09:39 +0100

    Day 3 Part 1 Done ⚙

---
+++ Solutions/Day3.cs
@@ -0,0 +1,101 @@
+﻿using Repository;
+
+namespace Solutions
+{
+    public class Day3 : AdventSolutionBase
+    {
+        private string Filename => $"{GetType().Name.ToLower()}.txt";
+
+        public Day3(IDataRetriever dataRetriever) : base(dataRetriever)
+        {
+        }
+
+        public override int FirstQuestion()
+        {
+            return FirstQuestion(Filename);
+        }
+
+        public override int FirstQuestion(string filename)
+        {
+            var allLines = GetAllLines(filename).ToList();
+            var sum = 0;
+            var isAdded = false;
+            for (int i = 0; i < allLines.Count; i++)
+            {
+                var concatNumberUpToJ = string.Empty;
+                for (int j = 0; j < allLines[i].Length; j++)
+                {
+                    var element = allLines[i][j];
+                    if (!char.IsDigit(element))
+                    {
+                        isAdded = false;
+                        concatNumberUpToJ = string.Empty;
+                        continue;
+                    }
+
+                    concatNumberUpToJ += element;
+                    if (!isAdded && HasAdjacentSymbol(i, j, allLines))
+                    {
+                        isAdded = true;
+                        sum += GetFullNumberAfterJ(concatNumberUpToJ, j, allLines[i]);
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        private int GetFullNumberAfterJ(string concatNumberUpToJ, int j, string line)
+        {
+            for (int k = j + 1; k < line.Length; k++)
+            {
+                if (!char.IsDigit(line[k])) return int.Parse(concatNumberUpToJ);
+                concatNumberUpToJ += line[k];
+            }
+
+            return int.Parse(concatNumberUpToJ);
+        }
+
+        private bool HasAdjacentSymbol(int i, int j, List<string> allLines)
+        {
+            // Check above
+            if (i > 0)
+            {
+                if (IsSymbol(allLines[i - 1][j])) return true;
+                if (j > 0 && IsSymbol(allLines[i - 1][j - 1])) return true;
+                if (j + 1 < allLines[i - 1].Length && IsSymbol(allLines[i - 1][j + 1])) return true;
+            }
+
+            // Check left and right
+            if (j > 0 && IsSymbol(allLines[i][j - 1])) return true;
+            if (j + 1 < allLines[i].Length && IsSymbol(allLines[i][j + 1])) return true;
+
+            // Check below
+            if (i + 1 < allLines.Count)
+            {
+                if (IsSymbol(allLines[i + 1][j])) return true;
+                if (j > 0 && IsSymbol(allLines[i + 1][j - 1])) return true;
+                if (j + 1 < allLines[i + 1].Length && IsSymbol(allLines[i + 1][j + 1])) return true;
+            }
+
+            return false;
+        }
+
+        private bool IsSymbol(char v)
+        {
+            if (v == '.') return false;
+            return !char.IsDigit(v);
+        }
+
+        public override int SecondQuestion()
+        {
+            return SecondQuestion(Filename);
+        }
+
+        public override int SecondQuestion(string filename)
+        {
+            var allLines = GetAllLines(filename);
+            return -1;
+        }
+    }
+}
---
+++ UnitTests/Day3Tests.cs
@@ -0,0 +1,54 @@
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+using Solutions;
+
+namespace UnitTests
+{
+    public class Day3Tests
+    {
+        private IAdventSolution adventDaySolution;
+        private string folderName = @"\Day3Examples";
+        [SetUp]
+        public void Setup()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IDataRetriever, TestDataRetriever>()
+                .AddSingleton<IAdventSolution, Day3>()
+                .BuildServiceProvider(validateScopes: true);
+            var scope = serviceProvider.CreateScope();
+
+            adventDaySolution = scope.ServiceProvider.GetRequiredService<IAdventSolution>();
+        }
+
+        [Test]
+        public void FirstQuestion_ShouldPass_Example()
+        {
+            var filename = folderName + @"\example1.txt";
+
+            var solution = adventDaySolution.FirstQuestion(filename);
+
+            solution.Should().Be(4361);
+        }
+
+        [Test]
+        public void FirstQuestion_ShouldPass_Example2()
+        {
+            var filename = folderName + @"\example2.txt";
+
+            var solution = adventDaySolution.FirstQuestion(filename);
+
+            solution.Should().Be(4361);
+        }
+
+        [Test]
+        public void FirstQuestion_ShouldPass_Example3()
+        {
+            var filename = folderName + @"\example3.txt";
+
+            var solution = adventDaySolution.FirstQuestion(filename);
+
+            solution.Should().Be(3403);
+        }
+    }
+}
