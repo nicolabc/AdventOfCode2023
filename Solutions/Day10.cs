210c6accbd3d9baa25ee870bce77af4fce35b191
nicolabc <31611304+nicolabc@users.noreply.github.com>
2023-12-12 22:10:59 +0100

    Day 10 Part 2 Done 🤯🗺🦝

--- Solutions/Day10.cs
+++ Solutions/Day10.cs
@@ -1,4 +1,6 @@
 ﻿using Repository;
+using System.Linq;
+using System.Text;
 using static Solutions.Day10;
 
 namespace Solutions
@@ -43,7 +45,7 @@ namespace Solutions
                 if (agents[0].NextPosition.Item1 == agents[1].i && agents[0].NextPosition.Item2 == agents[1].j) reachedFarthestPoint = true;
             }
 
-            return agents[0].stepCount;
+            return agents[0].StepCount;
         }
 
         public class Initializer
@@ -83,25 +85,90 @@ namespace Solutions
                 };
             }
         }
-       
-
 
         public class PipeMap
         {
             public List<string> map;
-            public string SouthFacingPipes => "|7F";
-            public string WestFacingPipes => "-J7";
-            public string NorthFacingPipes => "|LJ";
-            public string EastFacingPipes => "-LF";
 
             public PipeMap(List<string> map)
             {
                 this.map = map;
             }
 
-            public char GetPipe(int i, int j)
+            public char GetMapAt(int i, int j)
             {
                 return map[i][j];
+            }
+
+            public void UpdateMap(int i, int j, char c)
+            {
+                // Strings are immutable
+                StringBuilder sb = new StringBuilder(map[i]);
+                sb[j] = c;
+                map[i] = sb.ToString();
+            }
+
+            //(i,j) is the midpoint from which the update is performed
+            public bool UpdateMapInCardinalDirections(int i, int j, char c)
+            {
+                var updated = false;
+                if (i > 0 && GetMapAt(i - 1, j) == '.')
+                {
+                    UpdateMap(i - 1, j, c); 
+                    updated = true;
+                }
+                if (j > 0 && GetMapAt(i, j - 1) == '.')
+                {
+                    UpdateMap(i, j - 1, c); 
+                    updated = true;
+                }
+                if (i + 1 < map.Count && GetMapAt(i + 1, j) == '.')
+                {
+                    UpdateMap(i + 1, j, c); 
+                    updated = true;
+                }
+                if (j + 1 < map[i].Length && GetMapAt(i, j + 1) == '.')
+                {
+                    UpdateMap(i, j + 1, c); 
+                    updated = true;
+                }
+                return updated;
+            }
+
+            public bool UpdateMapInCardinalDirectionsForJunkPipes(int i, int j, char c, List<Position> mainLoop)
+            {
+                var updated = false;
+                if (i > 0 && !mainLoop.Contains(new Position { i = i - 1, j = j}) && GetMapAt(i - 1, j) != c)
+                {
+                    UpdateMap(i - 1, j, c);
+                    updated = true;
+                }
+                if (j > 0 && !mainLoop.Contains(new Position { i = i, j = j - 1 }) && GetMapAt(i, j - 1) != c)
+                {
+                    UpdateMap(i, j - 1, c);
+                    updated = true;
+                }
+                if (i + 1 < map.Count && !mainLoop.Contains(new Position { i = i + 1, j = j }) && GetMapAt(i + 1, j) != c)
+                {
+                    UpdateMap(i + 1, j, c);
+                    updated = true;
+                }
+                if (j + 1 < map[i].Length && !mainLoop.Contains(new Position { i = i, j = j + 1}) && GetMapAt(i, j + 1) != c)
+                {
+                    UpdateMap(i, j + 1, c);
+                    updated = true;
+                }
+                return updated;
+            }
+
+            public int GetCountOfChar(char charInsideLoop)
+            {
+                var count = 0;
+                foreach(var line in map)
+                {
+                    count += line.Count(x => x == charInsideLoop);
+                }
+                return count;
             }
         }
 
@@ -109,27 +176,29 @@ namespace Solutions
         {
             public int i;
             public int j;
-            public int stepCount;
-            public Direction currentDirection;
-            public PipeMap map;
+            public int StepCount;
+            public Direction CurrentDirection;
+            public Direction DirectionBeforeTurning;
+            public PipeMap Map;
 
             public Agent(int startingIndexI, int startingIndexJ, Direction startingDirection, PipeMap map)
             {
                 i = startingIndexI;
                 j = startingIndexJ;
-                stepCount = 0;
-                currentDirection = startingDirection;
-                this.map = map;
+                StepCount = 0;
+                CurrentDirection = startingDirection;
+                DirectionBeforeTurning = startingDirection;
+                this.Map = map;
             }
 
-            public void Move()
+            public virtual void Move()
             {
-                (i,j) = NextPosition;
-                currentDirection = GetNextDirection(currentDirection, map.GetPipe(i, j));
-                stepCount++;
+                (i, j) = NextPosition;
+                CurrentDirection = GetNextDirection(CurrentDirection, Map.GetMapAt(i, j));
+                StepCount++;
             }
 
-            public (int, int) NextPosition => currentDirection switch
+            public (int, int) NextPosition => CurrentDirection switch
             {
                 Direction.North => (i - 1, j),
                 Direction.East => (i, j + 1),
@@ -138,7 +207,7 @@ namespace Solutions
                 _ => throw new ArgumentOutOfRangeException(),
             };
 
-            private Direction GetNextDirection(Direction currentDirection, char pipe)
+            protected Direction GetNextDirection(Direction currentDirection, char pipe)
             {
                 var dir = currentDirection;
                 return pipe switch
@@ -149,6 +218,7 @@ namespace Solutions
                     'J' => dir == Direction.South ? Direction.West : dir == Direction.East ? Direction.North : throw new Exception($"Unexpected current direction {dir}"),
                     '7' => dir == Direction.North ? Direction.West : dir == Direction.East ? Direction.South : throw new Exception($"Unexpected current direction {dir}"),
                     'F' => dir == Direction.North ? Direction.East : dir == Direction.West ? Direction.South : throw new Exception($"Unexpected current direction {dir}"),
+                    'S' => Direction.South, // Does not matter
                     _ => throw new Exception($"Unknown pipe {pipe} reached with {dir}")
                 };
             }
@@ -173,8 +243,179 @@ namespace Solutions
 
         public override int SecondQuestion(string filename)
         {
-            var allLines = GetAllLines(filename);
-            return -1;
+            var allLines = GetAllLines(filename).ToList();
+            var (i, j) = GetStartingPoint(allLines);
+
+            var initializer = new Initializer(allLines);
+            var startingDirections = initializer.GetStartingPipeDirections(i, j);
+
+            var pipeMap = new PipeMap(allLines);
+            var agent = new AgentWithLRMarking(i, j, startingDirections.First(), pipeMap);
+
+            var loopedOneRound = false;
+            while (!loopedOneRound)
+            {
+                agent.Move();
+                if (agent.HasReachedStart())
+                {
+                    loopedOneRound = true;
+                }
+            }
+
+            var charInsideLoop = agent.GetNextDirectionOfTopLeftPipe();
+
+            var loopedSecondRound = false;
+            agent.SetStartingConditions(i, j, startingDirections.First());
+            while (!loopedSecondRound)
+            {
+                agent.MoveAndMarkLeftRight();
+                if (agent.HasReachedStart())
+                {
+                    loopedSecondRound = true;
+                }
+            }
+
+            var updatedInsideLoop = true;
+            while (updatedInsideLoop)
+            {
+                updatedInsideLoop = false;
+                var positions = GetPositionOfMarkings(agent.Map.map, charInsideLoop);
+                foreach (var position in positions)
+                {
+                    if(agent.UpdateMapInCardinalDirectionsForJunkPipes(position.i, position.j, charInsideLoop))
+                    {
+                        updatedInsideLoop = true;
+                    };
+                }
+            }
+            return agent.Map.GetCountOfChar(charInsideLoop);
+        }
+
+        public struct Position
+        {
+            public int i;
+            public int j;
+        }
+
+        private List<Position> GetPositionOfMarkings(List<string> allLines, char marking)
+        {
+            var i = 0;
+            var markingsPositions = new List<Position>();
+            foreach (var line in allLines)
+            {
+                for (var j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == marking)
+                    {
+                        markingsPositions.Add(new Position() { i = i, j = j });
+                    }
+                }
+
+                i++;
+            }
+            return markingsPositions;
+        }
+
+        public class AgentWithLRMarking : Agent
+        {
+            private Direction nextDirectionOfTopLeftPipe;
+            private int topLeftI;
+            private int topLeftJ;
+            private List<Position> loopPositions;
+
+            public AgentWithLRMarking(int startingIndexI, int startingIndexJ, Direction startingDirection, PipeMap map) : base(startingIndexI, startingIndexJ, startingDirection, map)
+            {
+                // Initialize values in case starting point is top left
+                nextDirectionOfTopLeftPipe = startingDirection;
+                topLeftI = startingIndexI;
+                topLeftJ = startingIndexJ;
+                loopPositions = new();
+            }
+
+            public bool HasReachedStart()
+            {
+                return Map.GetMapAt(i, j) == 'S';
+            }
+
+            public override void Move()
+            {
+                base.Move();
+                loopPositions.Add(new Position { i = i, j = j });
+                if (i < topLeftI || (i == topLeftI && j < topLeftJ))
+                {
+                    topLeftI = i;
+                    topLeftJ = j;
+                    nextDirectionOfTopLeftPipe = CurrentDirection;
+                }
+            }
+
+            public void MoveAndMarkLeftRight()
+            {
+                DirectionBeforeTurning = CurrentDirection;
+                base.Move();
+                MarkLeftAndRight();
+            }
+
+            public char GetNextDirectionOfTopLeftPipe()
+            {
+                return nextDirectionOfTopLeftPipe == Direction.East ? 'R' : nextDirectionOfTopLeftPipe == Direction.South ? 'B' : throw new Exception("Unexpected next direction");
+            }
+
+            private void MarkLeftAndRight()
+            {
+                MarkLeftAndRight(DirectionBeforeTurning);
+                MarkLeftAndRight(CurrentDirection);
+            }
+
+            private bool MarkLeftAndRight(Direction direction) => direction switch
+            {
+                Direction.North => MarkLeftAndRightWhenFacingNorth(),
+                Direction.East => MarkLeftAndRightWhenFacingEast(),
+                Direction.South => MarkLeftAndRightWhenFacingSouth(),
+                Direction.West => MarkLeftAndRightWhenFacingWest(),
+                _ => throw new NotImplementedException(),
+            };
+
+            private bool MarkLeftAndRightWhenFacingWest()
+            {
+                // Check range if they are outside the map and set 'B' and 'R' if not part of main loop
+                if (i + 1 < Map.map.Count && !loopPositions.Contains(new Position { i = i + 1, j = j})) Map.UpdateMap(i + 1, j, 'B');
+                if (i > 0 && !loopPositions.Contains(new Position { i = i - 1, j = j })) Map.UpdateMap(i - 1, j, 'R');
+                return true;
+            }
+
+            private bool MarkLeftAndRightWhenFacingSouth()
+            {
+                // Check range if they are outside the map and set 'B' and 'R' if not part of main loop
+                if (j + 1 < Map.map[i].Length && !loopPositions.Contains(new Position { i = i, j = j+1 })) Map.UpdateMap(i, j + 1, 'B');
+                if (j > 1 && !loopPositions.Contains(new Position { i = i, j = j - 1 })) Map.UpdateMap(i, j - 1, 'R');
+                return true;
+            }
+
+            private bool MarkLeftAndRightWhenFacingEast()
+            {
+                // Check range if they are outside the map and set 'B' and 'R' if not part of main loop
+                if (i + 1 < Map.map.Count && !loopPositions.Contains(new Position { i = i + 1, j = j })) Map.UpdateMap(i + 1, j, 'R');
+                if (i > 0 && !loopPositions.Contains(new Position { i = i - 1, j = j })) Map.UpdateMap(i - 1, j, 'B');
+                return true;
+            }
+
+            private bool MarkLeftAndRightWhenFacingNorth()
+            {
+                // Check range if they are outside the map and set 'B' and 'R' if not part of main loop
+                if (j + 1 < Map.map[i].Length && !loopPositions.Contains(new Position { i = i, j = j + 1 })) Map.UpdateMap(i, j + 1, 'R');
+                if (j > 1 && !loopPositions.Contains(new Position { i = i, j = j - 1 })) Map.UpdateMap(i, j - 1, 'B');
+                return true;
+            }
+
+            internal void SetStartingConditions(int i, int j, Direction direction)
+            {
+                this.i = i;
+                this.j = j;
+                CurrentDirection = direction;
+            }
+
+            public bool UpdateMapInCardinalDirectionsForJunkPipes(int i, int j, char c) => Map.UpdateMapInCardinalDirectionsForJunkPipes(i, j, c, loopPositions);
         }
     }
 
--- UnitTests/Day10Tests.cs
+++ UnitTests/Day10Tests.cs
@@ -40,5 +40,67 @@ namespace UnitTests
 
             solution.Should().Be(8);
         }
+
+        [Test]
+        public void SecondQuestion_ShouldPass_Example1()
+        {
+            var filename = folderName + @"\example1.txt";
+
+            var solution = adventDaySolution.SecondQuestion(filename);
+
+            solution.Should().Be(1);
+        }
+
+        [Test]
+        public void SecondQuestion_ShouldPass_Example3()
+        {
+            var filename = folderName + @"\example3.txt";
+
+            var solution = adventDaySolution.SecondQuestion(filename);
+
+            solution.Should().Be(4);
+        }
+
+        [Test]
+        public void SecondQuestion_ShouldPass_Example4()
+        {
+            var filename = folderName + @"\example4.txt";
+
+            var solution = adventDaySolution.SecondQuestion(filename);
+
+            solution.Should().Be(8);
+        }
+
+
+        [Test]
+        public void SecondQuestion_ShouldPass_Example5()
+        {
+            var filename = folderName + @"\example5.txt";
+
+            var solution = adventDaySolution.SecondQuestion(filename);
+
+            solution.Should().Be(10);
+        }
+
+        [Test]
+        public void SecondQuestion_ShouldPass_Example6()
+        {
+            var filename = folderName + @"\example6.txt";
+
+            var solution = adventDaySolution.SecondQuestion(filename);
+
+            solution.Should().Be(1);
+        }
+
+
+        [Test]
+        public void SecondQuestion_ShouldPass_Example7()
+        {
+            var filename = folderName + @"\example7.txt";
+
+            var solution = adventDaySolution.SecondQuestion(filename);
+
+            solution.Should().Be(2);
+        }
     }
 }
